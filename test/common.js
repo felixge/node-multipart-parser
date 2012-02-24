2fbbac1f5426f1a983af0e601804bf5a9d076325
Felix Geisendörfer <felix@debuggable.com>
2012-02-24 07:01:50 +0100

    Switch from fast-or-slow to utest
    Getting a failing test now, need to find out if it was failing before or
    not once I got internet and can use npm again.

--- test/common.js
+++ test/common.js
@@ -8,7 +8,6 @@ exports.dir = {
 };
 
 exports.assert = require('assert');
-exports.fastOrSlow = require('fast-or-slow');
 
 exports.require = function(lib) {
   return require(exports.dir.lib + '/' + lib);
--- test/fast/test-multipart-parser.js
+++ test/fast/test-multipart-parser.js
@@ -1,13 +1,196 @@
 var common          = require('../common');
-var test            = common.fastOrSlow.fast();
+var test            = require('utest');
 var assert          = common.assert;
 var MultipartParser = common.require('multipart_parser');
 var Part            = common.require('part');
 var boundary        = '------WebKitFormBoundarytyE4wkKlZ5CQJVTG';
 
 var parser;
-test.before(function() {
-  parser = MultipartParser.create(boundary);
+test('MultipartParser', {
+  before: function() {
+    parser = MultipartParser.create(boundary);
+  },
+
+  '#write: error: invalid parser state': function() {
+    parser._state = 'SOMETHING';
+    assertEmitsError(new Buffer('123'), 'MultipartParser.InvalidParserState');
+  },
+
+  '#write: error: write without boundary': function() {
+    var buffer = new Buffer('a');
+    parser     = new MultipartParser();
+
+    assert.throws(function() {
+      parser.write(buffer);
+    }, /Bad state: NO_BOUNDARY/);
+  },
+
+  '#write: tolerate missing CRLF on first boundary': function() {
+    var buffer = new Buffer('--' + boundary + '\r\n');
+    parser.write(buffer);
+
+    assert.equal(parser._state, 'HEADER_FIELD');
+  },
+
+  '#write: leading preamble': function() {
+    parser.write(new Buffer(boundary.substr(0, 4) + 'HAHA'));
+    assert.equal(parser._state, 'PREAMBLE');
+
+    parser.write(new Buffer('--' + boundary + '\r\n'));
+    assert.equal(parser._state, 'HEADER_FIELD');
+  },
+
+  '#write: error: Invalid header token': function() {
+      // ',' is an example for an invalid token for header fields (see RFC 2616)
+    var buffer = new Buffer('Invalid,Header: ');
+    parser._state = 'HEADER_FIELD';
+    assertEmitsError(buffer, 'MultipartParser.InvalidHeaderFieldToken');
+  },
+
+  '#write: Emit part object with lowercased headers': function() {
+    var buffer = new Buffer('Header-1:value-1\r\nHeader-2:value-2\r\n\r\n');
+    parser._state = 'HEADER_FIELD';
+    parser._part  = new Part();
+
+    parser.write(buffer);
+
+    assert.deepEqual(parser._part.headers, {
+      'header-1': 'value-1',
+      'header-2': 'value-2',
+    });
+  },
+
+  '#write: Trim leading and trailing header value whitespace': function() {
+    var buffer = new Buffer('header: value \r\n\r\n');
+    parser._state = 'HEADER_FIELD';
+    parser._part  = new Part();
+
+    parser.write(buffer);
+
+    assert.deepEqual(parser._part.headers, {'header': 'value'});
+  },
+
+  '#write: error: CR on non-empty _headerField': function() {
+    var buffer = new Buffer('head\r');
+    parser._state = 'HEADER_FIELD';
+    assertEmitsError(buffer, 'MultipartParser.InvalidHeaderFieldToken');
+  },
+
+  '#write: no part headers': function() {
+    var buffer = new Buffer('\r\n');
+    parser._state = 'HEADER_FIELD';
+    parser._part  = new Part();
+
+    parser.write(buffer);
+
+    assert.deepEqual(parser._part.headers, {});
+  },
+
+  '#write: header buffer overflow in field': function() {
+    parser._headerBufferLimit = 2;
+
+    var buffer = new Buffer('ab');
+    parser._state = 'HEADER_FIELD';
+    parser.write(buffer);
+    assertEmitsError(new Buffer('c'), 'MultipartParser.HeaderBufferOverflow');
+  },
+
+  '#write: emit part data': function() {
+    parser._part  = new Part();
+    parser._state = 'PART_BODY';
+
+    var expected = [
+      new Buffer('abc'),
+      new Buffer('def'),
+    ];
+
+    parser._part.on('data', function(buffer) {
+      assert.equal(''+buffer, ''+expected.shift());
+    });
+
+    parser.write(expected[0]);
+    parser.write(expected[0]);
+
+    assert.strictEqual(expected.length, 0);
+    assert.equal(parser._offset, 6);
+  },
+
+  '#write: hit partial boundary in part data': function() {
+    parser.boundary('end');
+    parser._preamble = false;
+    parser._part     = new Part();
+    parser._state    = 'PART_BODY';
+
+    var buffers =[];
+    parser._part.on('data', function(buffer) {
+      buffers.push(''+buffer);
+    });
+
+    parser.write(new Buffer('ab\r\n--enc'));
+    assert.deepEqual(buffers, ['ab', '\r\n--en', 'c']);
+  },
+
+  '#write: hit partial boundary in part data spread over 2 buffers': function() {
+    parser.boundary('end');
+    parser._preamble = false;
+    parser._part     = new Part();
+    parser._state    = 'PART_BODY';
+
+    var buffers =[];
+    parser._part.on('data', function(buffer) {
+      buffers.push(''+buffer);
+    });
+
+    var first = new Buffer('ab\r\n--e');
+    var second = new Buffer('haha');
+
+    parser.write(first);
+    assert.equal(buffers.length, 1);
+
+    parser.write(second);
+    assert.deepEqual(buffers, ['ab', '\r\n--e', 'haha']);
+  },
+
+  '#write: hit partial boundary in part data spread over 3 buffers': function() {
+    parser.boundary('end');
+    parser._preamble = false;
+    parser._part     = new Part();
+    parser._state    = 'PART_BODY';
+
+    var buffers =[];
+    parser._part.on('data', function(buffer) {
+      buffers.push(''+buffer);
+    });
+
+    var first = new Buffer('ab\r\n--e');
+    var second = new Buffer('n');
+    var third = new Buffer('haha');
+
+    parser.write(first);
+    assert.equal(buffers.length, 1);
+
+    parser.write(second);
+    assert.equal(buffers.length, 1);
+
+    parser.write(third);
+    assert.deepEqual(buffers, ['ab', '\r\n--en', 'haha']);
+  },
+
+  '#write: full rfc1341 entity': function() {
+    testRfc1341Entity();
+  },
+
+  '#write: full rfc1341 entity with chunk size 1-10': function() {
+    // What can I say, my ability to visualize this state machine has its limits :)
+    for (var i = 1; i <= 10; i++) {
+      try {
+        testRfc1341Entity(i);
+      } catch (err) {
+        err.message = 'Chunk size: ' + i + ': ' + err;
+        throw err;
+      }
+    }
+  }
 });
 
 function assertEmitsError(buffer, expectedError) {
@@ -20,171 +203,6 @@ function assertEmitsError(buffer, expectedError) {
   parser.write(buffer);
   assert.ok(hadError, 'no error was emitted');
 }
-
-test('#write: error: invalid parser state', function() {
-  parser._state = 'SOMETHING';
-  assertEmitsError(new Buffer('123'), 'MultipartParser.InvalidParserState');
-});
-
-test('#write: error: write without boundary', function() {
-  var buffer = new Buffer('a');
-  parser     = new MultipartParser();
-
-  assert.throws(function() {
-    parser.write(buffer);
-  }, /Bad state: NO_BOUNDARY/);
-});
-
-test('#write: tolerate missing CRLF on first boundary', function() {
-  var buffer = new Buffer('--' + boundary + '\r\n');
-  parser.write(buffer);
-
-  assert.equal(parser._state, 'HEADER_FIELD');
-});
-
-test('#write: leading preamble', function() {
-  parser.write(new Buffer(boundary.substr(0, 4) + 'HAHA'));
-  assert.equal(parser._state, 'PREAMBLE');
-
-  parser.write(new Buffer('--' + boundary + '\r\n'));
-  assert.equal(parser._state, 'HEADER_FIELD');
-});
-
-test('#write: error: Invalid header token', function() {
-    // ',' is an example for an invalid token for header fields (see RFC 2616)
-  var buffer = new Buffer('Invalid,Header: ');
-  parser._state = 'HEADER_FIELD';
-  assertEmitsError(buffer, 'MultipartParser.InvalidHeaderFieldToken');
-});
-
-test('#write: Emit part object with lowercased headers', function() {
-  var buffer = new Buffer('Header-1:value-1\r\nHeader-2:value-2\r\n\r\n');
-  parser._state = 'HEADER_FIELD';
-  parser._part  = new Part();
-
-  parser.write(buffer);
-
-  assert.deepEqual(parser._part.headers, {
-    'header-1': 'value-1',
-    'header-2': 'value-2',
-  });
-});
-
-test('#write: Trim leading and trailing header value whitespace', function() {
-  var buffer = new Buffer('header: value \r\n\r\n');
-  parser._state = 'HEADER_FIELD';
-  parser._part  = new Part();
-
-  parser.write(buffer);
-
-  assert.deepEqual(parser._part.headers, {'header': 'value'});
-});
-
-test('#write: error: CR on non-empty _headerField', function() {
-  var buffer = new Buffer('head\r');
-  parser._state = 'HEADER_FIELD';
-  assertEmitsError(buffer, 'MultipartParser.InvalidHeaderFieldToken');
-});
-
-test('#write: no part headers', function() {
-  var buffer = new Buffer('\r\n');
-  parser._state = 'HEADER_FIELD';
-  parser._part  = new Part();
-
-  parser.write(buffer);
-
-  assert.deepEqual(parser._part.headers, {});
-});
-
-test('#write: header buffer overflow in field', function() {
-  parser._headerBufferLimit = 2;
-
-  var buffer = new Buffer('ab');
-  parser._state = 'HEADER_FIELD';
-  parser.write(buffer);
-  assertEmitsError(new Buffer('c'), 'MultipartParser.HeaderBufferOverflow');
-});
-
-test('#write: emit part data', function() {
-  parser._part  = new Part();
-  parser._state = 'PART_BODY';
-
-  var expected = [
-    new Buffer('abc'),
-    new Buffer('def'),
-  ];
-
-  parser._part.on('data', function(buffer) {
-    assert.equal(''+buffer, ''+expected.shift());
-  });
-
-  parser.write(expected[0]);
-  parser.write(expected[0]);
-
-  assert.strictEqual(expected.length, 0);
-  assert.equal(parser._offset, 6);
-});
-
-test('#write: hit partial boundary in part data', function() {
-  parser.boundary('end');
-  parser._preamble = false;
-  parser._part     = new Part();
-  parser._state    = 'PART_BODY';
-
-  var buffers =[];
-  parser._part.on('data', function(buffer) {
-    buffers.push(''+buffer);
-  });
-
-  parser.write(new Buffer('ab\r\n--enc'));
-  assert.deepEqual(buffers, ['ab', '\r\n--en', 'c']);
-});
-
-test('#write: hit partial boundary in part data spread over 2 buffers', function() {
-  parser.boundary('end');
-  parser._preamble = false;
-  parser._part     = new Part();
-  parser._state    = 'PART_BODY';
-
-  var buffers =[];
-  parser._part.on('data', function(buffer) {
-    buffers.push(''+buffer);
-  });
-
-  var first = new Buffer('ab\r\n--e');
-  var second = new Buffer('haha');
-
-  parser.write(first);
-  assert.equal(buffers.length, 1);
-
-  parser.write(second);
-  assert.deepEqual(buffers, ['ab', '\r\n--e', 'haha']);
-});
-
-test('#write: hit partial boundary in part data spread over 3 buffers', function() {
-  parser.boundary('end');
-  parser._preamble = false;
-  parser._part     = new Part();
-  parser._state    = 'PART_BODY';
-
-  var buffers =[];
-  parser._part.on('data', function(buffer) {
-    buffers.push(''+buffer);
-  });
-
-  var first = new Buffer('ab\r\n--e');
-  var second = new Buffer('n');
-  var third = new Buffer('haha');
-
-  parser.write(first);
-  assert.equal(buffers.length, 1);
-
-  parser.write(second);
-  assert.equal(buffers.length, 1);
-
-  parser.write(third);
-  assert.deepEqual(buffers, ['ab', '\r\n--en', 'haha']);
-});
 
 function testRfc1341Entity(chunkSize) {
   parser.boundary('simple boundary');
@@ -255,16 +273,4 @@ function testRfc1341Entity(chunkSize) {
     assert.ok(part.ended, 'Part ' + (i + 1) + ' did not end.');
   });
   assert.ok(ended);
-}
-
-test('#write: full rfc1341 entity', function() {
-  testRfc1341Entity();
-});
-
-// What can I say, my ability to visualize this state machine has its limits :)
-for (var i = 1; i <= 10; i++) {
-  test('#write: full rfc1341 entity with chunk size: ' + i, function() {
-    var chunkSize = parseInt(this.name.match(/\d+$/), 10);
-    testRfc1341Entity(chunkSize);
-  });
 }
